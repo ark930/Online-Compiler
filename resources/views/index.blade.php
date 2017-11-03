ebde9f9ee1d2f8dd7df8aee05bb407646eae7a53
Edwin Wang <lark930@gmail.com>
2017-11-04 00:14:45 +0800

    更新页面结构

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,58 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>ByteWave</title>
-  <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <link rel="stylesheet" href="{{ asset('css/xterm.css') }}" />
-  <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
-  <![endif]-->
-</head>
-<body>
-<nav class="navbar main">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">ByteWave在线编译</a>
-    </div>
+@extends('layouts.app')
 
-    <ul class="nav navbar-nav navbar-right">
-      @guest
-        <li><a href="{{ route('login') }}">登录</a></li>
-        <li><a href="{{ route('register') }}">注册</a></li>
-      @else
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-            {{ Auth::user()->name }}<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">个人中心</a></li>
-            <li><a href="#">回到首页</a></li>
-            <li role="separator" class="divider"></li>
-            <li>
-              <a href="{{ route('logout') }}"
-                 onclick="event.preventDefault();
-                 document.getElementById('logout-form').submit();">
-                退出
-              </a>
+@section('css')
+  <link rel="stylesheet" href="{{ asset('css/xterm.css') }}"/>
+@endsection
 
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                {{ csrf_field() }}
-              </form>
-            </li>
-          </ul>
-        </li>
-      @endguest
-    </ul>
-  </div>
-</nav>
-
+@section('content')
 <div class="main-content">
   <nav class="navbar sub">
     <div class="container-fluid">
@@ -142,42 +94,42 @@
     </div>
   </div>
 </div>
+@endsection
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="{{ asset('js/xterm.js') }}"></script>
-<script src="{{ asset('js/addons/attach/attach.js') }}"></script>
-<script src="{{ asset('js/index.js') }}"></script>
-<script>
-    var term = new Terminal();
-    term.open(document.getElementById('terminal'));
+@section('footer_script')
+  <script src="{{ asset('js/xterm.js') }}"></script>
+  <script src="{{ asset('js/addons/attach/attach.js') }}"></script>
+  <script src="{{ asset('js/index.js') }}"></script>
+  <script>
+      var term = new Terminal();
+      term.open(document.getElementById('terminal'));
 
-    var protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
-    var socketURL = protocol + location.hostname + ((location.port) ? (':' + '3000') : '') + '/terminals/';
-    socketURL += {{ session()->get('pid') }};
-    var socket = new WebSocket(socketURL);
-    socket.onopen = runRealTerminal;
-//    $.ajax({
-//        url: "http://127.0.0.1:3000/terminals",
-//        method: 'post',
-//        data: {},
-//        success: function(result) {
-//            var pid = result;
-//
-//        },
-//        error: function (result) {
-//            console.log('error', result)
-//        }
-//    });
+      var protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
+      var socketURL = protocol + location.hostname + ((location.port) ? (':' + '3000') : '') + '/terminals/';
+      socketURL += {{ session()->get('pid') }};
+      var socket = new WebSocket(socketURL);
+      socket.onopen = runRealTerminal;
+  //    $.ajax({
+  //        url: "http://127.0.0.1:3000/terminals",
+  //        method: 'post',
+  //        data: {},
+  //        success: function(result) {
+  //            var pid = result;
+  //
+  //        },
+  //        error: function (result) {
+  //            console.log('error', result)
+  //        }
+  //    });
 
-//    socket.onclose = runFakeTerminal;
-//    socket.onerror = runFakeTerminal;
+  //    socket.onclose = runFakeTerminal;
+  //    socket.onerror = runFakeTerminal;
 
-    function runRealTerminal() {
-        term.attach(socket);
-        term._initialized = true;
-    }
-</script>
+      function runRealTerminal() {
+          term.attach(socket);
+          term._initialized = true;
+      }
+  </script>
+@endsection
 </body>
 </html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,57 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>ByteWave在线编译</title>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>ByteWave在线编译</title>
+  <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+  @yield('css')
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        ByteWave在线编译
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">登录</a></li>
-                            <li><a href="{{ route('register') }}">注册</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        @yield('content')
+<div id="app">
+  <nav class="navbar main">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="/">ByteWave在线编译</a>
+      </div>
+      <ul class="nav navbar-nav navbar-right">
+        @guest
+          <li><a href="{{ route('login') }}">登录</a></li>
+          <li><a href="{{ route('register') }}">注册</a></li>
+        @else
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+               aria-expanded="false">
+              {{ Auth::user()->name }}<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">个人中心</a></li>
+              {{--<li><a href="#">回到首页</a></li>--}}
+              <li role="separator" class="divider"></li>
+              <li>
+                <a href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                   document.getElementById('logout-form').submit();">
+                  退出
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  {{ csrf_field() }}
+                </form>
+              </li>
+            </ul>
+          </li>
+        @endguest
+      </ul>
     </div>
+  </nav>
+  @yield('content')
+</div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+@yield('footer_script')
 </body>
 </html>
