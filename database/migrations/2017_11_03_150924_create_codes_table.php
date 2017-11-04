15c13b74c202cd0bba222fc68eeca3dc362f822b
Edwin Wang <lark930@gmail.com>
2017-11-04 10:58:15 +0800

    新增代码表

---
+++ database/migrations/2017_11_03_150924_create_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->enum('language', ['c', 'c++', 'java', 'python27', 'python3', 'php']);
+            $table->text('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codes');
+    }
+}
