58b19f94364c6d9e416c8f452824cab277d2d33a
ark930 <lark930@gmail.com>
2017-11-19 15:19:55 +0800

    新增xterm服务器代码

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('type', ['student', 'teacher'])->nullable();
+            $table->string('name')->nullable();
+            $table->string('school')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ xterm/app.js
@@ -0,0 +1,38 @@
+// var cors = require('cors')
+var express = require('express');
+var app = express();
+var os = require('os');
+var pty = require('node-pty');
+
+var terminals = {},
+    logs = {};
+
+// app.use(cors());
+app.use('/build', express.static(__dirname + '/build'));
+
+app.post('/terminals', function (req, res) {
+  var cols = parseInt(req.query.cols),
+      rows = parseInt(req.query.rows),
+      term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'zsh', [], {
+        name: 'xterm-color',
+        cols: cols || 80,
+        rows: rows || 24,
+        cwd: process.env.PWD,
+        env: process.env
+      });
+  console.log('Created terminal with PID: ' + term.pid);
+  terminals[term.pid] = term;
+  logs[term.pid] = '';
+  term.on('data', function(data) {
+    logs[term.pid] += data;
+  });
+  
+  res.send(term.pid.toString());
+  res.end();
+});
+
+var port = process.env.PORT || 3000,
+    host = os.platform() === 'win32' ? '127.0.0.1' : '0.0.0.0';
+
+console.log('App listening to http://' + host + ':' + port);
+app.listen(port, host);
