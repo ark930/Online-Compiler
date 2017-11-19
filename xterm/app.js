f152473b2dbba90b691ca857e0121dbb2751835c
ark930 <lark930@gmail.com>
2017-11-19 16:00:23 +0800

    xterm使用bash终端

--- xterm/app.js
+++ xterm/app.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res){
 app.post('/terminals', function (req, res) {
   var cols = parseInt(req.query.cols),
       rows = parseInt(req.query.rows),
-      term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'zsh', [], {
+      term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
         name: 'xterm-color',
         cols: cols || 80,
         rows: rows || 24,
@@ -60,7 +60,7 @@ app.ws('/terminals/:pid', function (ws, req) {
 });
 
 var port = process.env.PORT || 3000,
-    host = os.platform() === 'win32' ? '127.0.0.1' : '0.0.0.0';
+    host = '127.0.0.1';
 
 console.log('App listening to http://' + host + ':' + port);
 app.listen(port, host);
